63aa8fd37d9b8fcc3e0cc973a6bb469a144594ee
titi9011 <t1thierry@hotmail.com>
2023-08-22 22:38:52 -0400

    Initial commit

---
+++ Source/Musso/MussoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MussoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUSSO_API AMussoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
